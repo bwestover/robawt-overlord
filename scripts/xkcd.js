378f6c3f87530e7413e19d5cd6a9e8c8715a1ca1
bwestover <bwestover@github.com>
2018-01-27 09:05:40 -0800

    ugh parameters go inside the parenthesis

--- scripts/xkcd.js
+++ scripts/xkcd.js
@@ -6,7 +6,7 @@
 module.exports = robot =>
 
   robot.respond(/xkcd\b/i, function(msg) {
-    robot.logger "Hello World"
+    robot.logger.debug("Hello World");
     msg.http("https://xkcd.com/614/info.0.json")
       .get()(function(err, res, body) {
          robot.logger.debug(body);
