930b37c370eb58090b1ca92a2706021bf7163a96
bwestover <bwestover@github.com>
2018-01-27 14:05:10 -0800

    Add some yoda goodness to the weather.

---
+++ scripts/yoda-weather.js
@@ -0,0 +1,39 @@
+// Description:
+//   Return the weather in Yoda speak
+//
+// Commands:
+//   hubot yoda-weather <location> - returns weather for <location> in Yoda speak
+module.exports = robot =>
+
+  robot.respond(/yoda-weather\b ?(.*)/i, function(msg) {
+
+    const location = msg.match[1];
+
+    //Pull forecast from wunderground
+    forecast = msg.http(`http://api.wunderground.com/api/${process.env.HUBOT_WUNDERGROUND_API_KEY}/forecast/q/${encodeURIComponent(location)}.json`)
+      .get()(function(err, res, body) {
+           robot.logger.info(body);
+           if (err) {
+             robot.logger.error(err);
+             return robot.emit('error', err, msg);
+           }
+
+      data = JSON.parse(body)
+      report = data.forecast.txt_forecast.forecastday[0]
+      return `${report.title} in ${location} there will be ${report.fcttext}.`
+    });
+
+    //Return yoda speak
+    msg.http(`https://yoda.p.mashape.com/yoda?sentence=${encodeURIComponent(forecast)}`)
+      .header('X-Mashape-Key',`${process.env.HUBOT_MASHAPE_KEY}`)
+      .get()(function(err, res, body) {
+           robot.logger.info(body);
+           if (err) {
+             robot.logger.error(err);
+             return robot.emit('error', err, msg);
+           }
+
+      yodasays = JSON.parse(body)
+      msg.send(yodasays)
+    });
+  });
