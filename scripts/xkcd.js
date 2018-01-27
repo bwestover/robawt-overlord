00f9bb201b3c5d1874e3c3466b091aa8c8d4a4ba
bwestover <bwestover@github.com>
2018-01-27 09:23:29 -0800

    use info logger

--- scripts/xkcd.js
+++ scripts/xkcd.js
@@ -6,10 +6,10 @@
 module.exports = robot =>
 
   robot.respond(/xkcd\b/i, function(msg) {
-    robot.logger.debug("Hello World");
+    robot.logger.info("Hello World");
     msg.http("https://xkcd.com/614/info.0.json")
       .get()(function(err, res, body) {
-         robot.logger.debug(body);
+         robot.logger.info(body);
          if (err) {
            robot.logger.error(err);
            return robot.emit('error', err, msg);
