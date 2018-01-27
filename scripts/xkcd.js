947b2ee9b019905bac9b470a2e52c4659d1fe3fc
bwestover <bwestover@github.com>
2018-01-27 09:02:25 -0800

    xkcd

---
+++ scripts/xkcd.js
@@ -0,0 +1,21 @@
+// Description:
+//   Return XKCD comics
+//
+// Commands:
+//   hubot xckd - returns a random XKCD comics
+module.exports = robot =>
+
+  robot.respond(/xkcd\b/i, function(msg) {
+    robot.logger "Hello World"
+    msg.http("https://xkcd.com/614/info.0.json")
+      .get()(function(err, res, body) {
+         robot.logger.debug(body);
+         if (err) {
+           robot.logger.error(err);
+           return robot.emit('error', err, msg);
+         }
+         comic = JSON.parse(body).img;
+         robot.logger.debug(comic);
+    return msg.send(comic);
+  })
+;
