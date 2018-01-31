dcdc2bdf65d23197638f68761841120cf64953b2
bwestover <bwestover@github.com>
2018-01-30 22:10:31 -0800

    Take out mock responses and use the real API

--- scripts/yoda-weather.js
+++ scripts/yoda-weather.js
@@ -9,37 +9,32 @@ module.exports = robot =>
 
     const location = msg.match[1];
     robot.logger.info(location)
-    if (location == 'salida, ca'){
-      msg.send("Today in salida, rainy, ca it will be.  Hmmmmmm.");;
-    } else if (location == 'boston, ma'){
-      msg.send("Today in boston, showers possible will ma it be.  Lows overnight in the mid 40s.  Yeesssssss.");
-    }
 
-//     //Pull forecast from wunderground
-//     forecast = msg.http(`http://api.wunderground.com/api/${process.env.HUBOT_WUNDERGROUND_API_KEY}/forecast/q/${encodeURIComponent(location)}.json`)
-//       .get()(function(err, res, body) {
-// //           robot.logger.info(body);
-//            if (err) {
-//              robot.logger.error(err);
-//              return robot.emit('error', err, msg);
-//            }
-//
-//       data = JSON.parse(body)
-//       report = data.forecast.txt_forecast.forecastday[0]
-//       return `${report.title} in ${location} there will be ${report.fcttext}.`
-//     });
-//
-//     //Return yoda speak
-//     msg.http(`https://yoda.p.mashape.com/yoda?sentence=${encodeURIComponent(forecast)}`)
-//       .header('X-Mashape-Key',`${process.env.HUBOT_MASHAPE_KEY}`)
-//       .get()(function(err, res, body) {
-// //           robot.logger.info(body);
-//            if (err) {
-//              robot.logger.error(err);
-//              return robot.emit('error', err, msg);
-//            }
-//
-//       yodasays = body
-//       msg.send(yodasays)
-//     });
+    //Pull forecast from wunderground
+    forecast = msg.http(`http://api.wunderground.com/api/${process.env.HUBOT_WUNDERGROUND_API_KEY}/forecast/q/${encodeURIComponent(location)}.json`)
+      .get()(function(err, res, body) {
+//           robot.logger.info(body);
+           if (err) {
+             robot.logger.error(err);
+             return robot.emit('error', err, msg);
+           }
+
+      data = JSON.parse(body)
+      report = data.forecast.txt_forecast.forecastday[0]
+      return `${report.title} in ${location} there will be ${report.fcttext}.`
+    });
+
+    //Return yoda speak
+    msg.http(`https://yoda.p.mashape.com/yoda?sentence=${encodeURIComponent(forecast)}`)
+      .header('X-Mashape-Key',`${process.env.HUBOT_MASHAPE_KEY}`)
+      .get()(function(err, res, body) {
+//           robot.logger.info(body);
+           if (err) {
+             robot.logger.error(err);
+             return robot.emit('error', err, msg);
+           }
+
+      yodasays = body
+      msg.send(yodasays)
+    });
    });
